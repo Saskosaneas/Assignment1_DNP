acdbcf44c001b2047475a92f0a6c0d6ed9de1535
Saskosaneas <chilatsandu@gmail.com>
2024-09-19 21:59:44 +0200

    first step, going good I guess

---
+++ Server/CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿using CLI.UI;
+using InMemoryRepositories;
+using RepositoryContracts;
+
+Console.WriteLine("Starting CLI app...");
+IuserRepository userRepository = new UserInMemoryRepository();
+IcommentRepository commentRepository = new CommentInMemoryRepository();
+IpostReporsitory postReporsitory = new PostInMemoryRepository();
+
+CliApp cliApp = new CliApp(userRepository, commentRepository, postReporsitory);
+await cliApp.StartAsync();
---
+++ Server/CLI/UI/CliApp.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using InMemoryRepositories;
+using RepositoryContracts;
+namespace CLI.UI;
+
+public class CliApp
+{
+    private readonly IuserRepository _userRepository;
+    private readonly IcommentRepository _commentRepository;
+    private readonly IpostReporsitory _postRepository;
+
+    public CliApp(IuserRepository userRepository, IcommentRepository commentRepository, IpostReporsitory postRepository)
+    {
+        _userRepository = userRepository;
+        _commentRepository = commentRepository;
+        _postRepository = postRepository;
+    }
+
+    public async Task StartAsync()
+    {   
+        Console.WriteLine("fdafadafa........");
+    }
+}
+
