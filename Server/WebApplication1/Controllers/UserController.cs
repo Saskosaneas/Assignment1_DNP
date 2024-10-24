44a0b0c701f1839d73d6972a9ecd35f9a6e97633
Saskosaneas <chilatsandu@gmail.com>
2024-10-24 21:18:36 +0200

    still working ... be back at it, dont quite understand the controllers for now

---
+++ Server/WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using DTOs;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryContracts;
+
+namespace WebApplication1.Controllers;
+[ApiController]
+[Route("[controller]")]
+
+public class UserController:ControllerBase
+{
+    private readonly IuserRepository userRepository;
+
+    public UserController(IuserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+    
+    
+[HttpPost]
+public async Task<ActionResult<UserDto>> AddUser([FromBody] UserDto req, [FromServices] IuserRepository userRepository)
+{
+    User user = new User(req.Username, req.Password);
+    User created = await userRepository.AddAsync(user);
+        UserDto = new()
+    {
+        Username = created.Username,
+        Password = created.Password
+    };
+        
+    return Created($"users/{created.ID}", DTOs.UserDto);
+}
+
+
+}
